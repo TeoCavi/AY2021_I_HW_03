a9aa1d57d0981c08636116acde042fcad2051c0b
Matteo <matteo.cavicchioli96@gmail.com>
2020-10-15 21:41:30 +0200

    UART ok, Timer instabile

--- CAVICCHIOLI_MATTEO.cydsn/InterruptRoutines.c
+++ CAVICCHIOLI_MATTEO.cydsn/InterruptRoutines.c
@@ -12,18 +12,19 @@
 #include "InterruptRoutines.h"
 #include "stdio.h"
 #include "UART.h"
+#include "TIMER_UART.h"
 
 CY_ISR(Custom_UART_ISR)
 {
     if(UART_ReadRxStatus() == UART_RX_STS_FIFO_NOTEMPTY) // && flag_TIMER == 0)
-        flag_UART ++;
+        flag_UART = 1;
 }
 
-/*CY_ISR_PROTO(Custom_TIMER_ISR);
+CY_ISR(Custom_TIMER_ISR)
 {
-    if(UART_ReadRxStatus() != UART_RX_STS_FIFO_NOTEMPTY && flag_UART == 1)
-        flag_TIMER = 1;
-}*/
+    flag_TIMER = 0; 
+    TIMER_UART_ReadStatusRegister();
+}
 
     
 
--- CAVICCHIOLI_MATTEO.cydsn/main.c
+++ CAVICCHIOLI_MATTEO.cydsn/main.c
@@ -10,31 +10,91 @@
  * ========================================
 */
 #include "project.h"
+#include "stdio.h"
 #include "InterruptRoutines.h"
 #include "globalvariables.h"
 
 uint8 flag_UART = 0;
+uint8 flag_TIMER = 1;
 
 
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
     
-    int start = 0;
+    
+    uint8 data;
+    uint8 RGB[3] = {'\0'};
+    int start = 0, i = 0;
 
     UART_Start();
     ISR_UART_StartEx(Custom_UART_ISR);
-    //ISR_TIMER_StartEx(Custom_TIMER_ISR);
+    ISR_TIMER_StartEx(Custom_TIMER_ISR);
+    CLK_TIMER_Start();
+    
     
 
     for(;;)
     {
-        if (flag_UART == 1)
+        if (flag_UART == 1 && flag_TIMER == 1)
         {
-            if(UART_ReadRxData() == 0xA0)
+            data = UART_ReadRxData();
+            if (data == 'v' && i == 0)
+            {
+                UART_PutString("RGB LED Program $$$\r\n");
+            }
+            else if( data == 0xA0 && i == 0)
             {
                 start = 1;
+                UART_PutString(" Start ");
+                TIMER_UART_Init();
+                TIMER_UART_Enable();
+                i = 1;
             }
+            else if ( data == 0xC0)
+            {
+                start = 0;
+                UART_ClearRxBuffer();
+                UART_PutString(" End ");
+                TIMER_UART_Stop();
+                flag_TIMER = 1;
+                i = 0;
+            }
+            else if (start == 1)
+            {
+                RGB[i] = data;
+                UART_PutChar(RGB[i]);
+                TIMER_UART_Stop();
+                TIMER_UART_Init();
+                TIMER_UART_Enable();
+
+                i ++;
+            }
+            flag_UART = 0;
+        }
+        else if (start == 1 && flag_TIMER == 0)
+        {
+            start = 0;
+            UART_ClearRxBuffer();
+            TIMER_UART_Stop();
+            flag_TIMER = 1;
+            UART_PutString(" To Much Time, Repeat ");
+            i = 0;
+        }
+
+            
+        
+                
+            
+            
+        
+        
+                
+                
+                
+                
+                
+            
             
             
             
